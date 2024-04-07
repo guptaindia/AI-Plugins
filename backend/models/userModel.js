3de7e2bc214989bb1796637be0dde46745f0d0ac
priyanshidwivedi25 <priyanshudwivedi448@gmail.com>
2024-04-07 13:56:47 +0530

    ....

---
+++ backend/models/userModel.js
@@ -0,0 +1,16 @@
+const {Schema, model} = require('../connection');
+
+const mySchema  =  new Schema({
+
+    title:{type: String, required: true},
+    name : String,
+    email :String,
+    password :String,
+    role: String,
+    createdAt: Date, 
+    // Avatar :{type:Number , default : 0},
+
+
+})
+
+module.exports = model('usercollection',mySchema);
