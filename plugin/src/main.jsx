000e2ea206ef606e2473633ed6f7d6b0d575a913
Himanshu <guptarajeshwarionejan@gmail.com>
2024-04-20 16:03:02 +0530

    testplugin added

--- plugin/src/main.jsx
+++ plugin/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import GestureRecognitionPlugin from './GestureRecognition.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
+// let gestureEle = document.getElementById('gesture-recognition-plugin');
+// if (gestureEle) {
+//   ReactDOM.createRoot(gestureEle).render(<GestureRecognitionPlugin />)
+// }
