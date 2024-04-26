deafa9544d2e7439323be2d8de235590b244073a
Himanshu <guptarajeshwarionejan@gmail.com>
2024-04-26 23:27:14 +0530

    home page styling

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className=''>
       <Wavyground title={'Rapidly use modern AI model without ever leaving your code.'} description={'A javascript framework packed with models like gesture, semantic, speech and object detection that can be composed to build on your code, directly on website.'}/>
-      <h1 className='bg-neutral-900 text-center text-white font-bold text-5xl'>Features</h1>
+      <h1 className='text-2xl md:text-4xl lg:text-5xl text-black font-bold inter-var text-center'>Features</h1>
       <CardHoverEffectDemo/>
     </div>
   )
--- frontend/src/app/wavy-background.jsx
+++ frontend/src/app/wavy-background.jsx
@@ -8,10 +8,10 @@ export default function Wavyground({title, description}) {
     <div>
       
     <WavyBackground className="max-w-4xl mx-auto pb-80 ">
-      <p className="text-2xl md:text-4xl lg:text-5xl text-white font-bold inter-var text-center">
+      <p className="text-2xl md:text-4xl lg:text-5xl text-black font-bold inter-var text-center">
         {title}
       </p>
-      <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+      <p className="text-base md:text-lg mt-4 text-black font-normal inter-var text-center">
         {description}
       </p>
     </WavyBackground>
