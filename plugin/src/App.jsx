b170ff7218527ab5cd66cdac9e29177a0e7c619a
Himanshu <guptarajeshwarionejan@gmail.com>
2024-04-27 00:16:46 +0530

    backkend regidtry error

--- plugin/src/App.jsx
+++ plugin/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 //import ToxicityDetection from './toxicityDetection'
 import ObjDetect from './objectDetection'
+//import Dictaphone1 from './speechRecognition'
 
 const App = () => {
   return (
     <div>
       <ObjDetect />
+      {/* <Dictaphone1/> */}
     </div>
   )
 }
--- plugin/src/objectDetection.jsx
+++ plugin/src/objectDetection.jsx
@@ -4,6 +4,7 @@ import React, { useRef, useEffect } from "react";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 //import "./App.css";
+
 import { drawRect } from "./utils";
 
 function ObjDetect() {
