5a67f7d59aca30351fcc5e3ef6ae747e1dd2ff12
Himanshu <guptarajeshwarionejan@gmail.com>
2024-05-22 13:55:59 +0530

    avatar

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div >
+    <div>
       <div class="relative bg-[#111827]">
         <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
           <div class="relative flex items-center justify-between">
@@ -121,10 +121,69 @@ const Navbar = () => {
                   About us
                 </a>
               </li>
-            </ul>
-             {
-              showLoggedIn()
-             }
+            </ul>       
+  <div style={{zIndex: 100}}>
+                <img
+                  id="avatarButton"
+                  type="button"
+                  data-dropdown-toggle="userDropdown"
+                  data-dropdown-placement="bottom-start"
+                  className="w-10 h-10 rounded-full cursor-pointer"
+                  src="/cardImg2.jpg"
+                  alt="User dropdown"
+                />
+                {/* Dropdown menu */}
+                <div
+                  id="userDropdown"
+                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                >
+                  <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                    <div>Bonnie Green</div>
+                    <div className="font-medium truncate">name@flowbite.com</div>
+                  </div>
+                  <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="avatarButton"
+                  >
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Dashboard
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Settings
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Earnings
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="py-1">
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Sign out
+                    </a>
+                  </div>
+                </div>
+                {
+                  showLoggedIn()
+                }
+  </div>
+
             <div class="lg:hidden" style={{zIndex: 100}}>
               <button
                 aria-label="Open Menu"
